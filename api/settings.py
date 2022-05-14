92183a3fe09409d5fadbabb471a28fe0f6d55045
chrisdurning22 <chrisdurning18@gmail.com>
2022-05-14 22:59:55 +0100

    updated settings.py

--- api/settings.py
+++ api/settings.py
@@ -16,6 +16,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
